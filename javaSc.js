75e2e178fd2c83f69755ff1d8770d9629dddf0b5
sajib <shkaisar2002@gmail.com>
2023-11-01 00:31:10 +0600

    12th

--- javaSc.js
+++ javaSc.js
@@ -1,15 +1,46 @@
-
 var xx=new Typed(".dynamic",{
     strings: ["C++","Python","Java"],
     typeSpeed:150,
     backSpeed:150,
     loop: true
     })
+
+
+    // load form
+
+    const rr=document.querySelector("#registration .form");
+    window.addEventListener("load",function(){
+        showPopup();
+    })
+    function showPopup(){
+        const timeLimit=4
+        let i=0;
+        const timer=setInterval(function(){
+            i++;
+            if(i==timeLimit){
+                clearInterval(timer);
+                rr.classList.add("show");
+            }
+            console.log(i)
+        },1000)
+    }
+
+    // menu click
+
+    $('#menu-btn').click(function(){
+        $('nav .navigation ul').addClass('active')
+    });
     
+
+    $('#menu-close').click(function(){
+        $('nav .navigation ul').removeClass('active' )
+    });
+    
+
 
     // login new window
 
-function func(){
+    function func(){
     var user=document.getElementById("username").value;
     var pass=document.getElementById('password').value;
 
